1adc2914e468234170891f0318c2284501f81d27
Angela <lik.bobrova@yandex.ru>
2021-12-23 13:17:39 +0300

    Small changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String cityName = "Moscow";
+        String cityName = "Kazan";
         var cityId = RequestWeather.getCityIdRequest(cityName);
         System.out.println(RequestWeather.getCityIdRequest(cityName));
         System.out.println(RequestWeather.getOneDailyWeatherCityRequest(cityId));
