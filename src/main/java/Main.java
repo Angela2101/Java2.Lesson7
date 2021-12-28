4c520494f75f0d8cc5acc2e9a670f360e27570d3
Angela <lik.bobrova@yandex.ru>
2021-12-28 11:52:11 +0300

    Changes for Lesson 8

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws IOException, SQLException {
-        String cityName = "Kazan";
+        String cityName = "Penza";
         var cityId = RequestWeather.getCityIdRequest(cityName);
         System.out.println(RequestWeather.getCityIdRequest(cityName));
         var weather  = RequestWeather.getOneDailyWeatherCityRequest(cityId, cityName);
